90671fc8bed3f1fd7b23307987229466c0e2c62e
vilda.novak@gmail.com <vilda.novak@gmail.com@d56605d3-40e8-9911-096e-8b66a8177f32>
2013-05-26 14:52:38 +0000

    adding presets. most of them were provided by Bernhard.

---
+++ trunk/scripts/presets/cam_cutters/ball_7.00mm.py
@@ -0,0 +1,7 @@
+import bpy
+d = bpy.context.scene.cam_operations[bpy.context.scene.cam_active_operation]
+
+d.cutter_type = 'BALL'
+d.cutter_diameter = 0.0070
+d.cutter_length = 25.0
+d.cutter_tip_angle = 60.0
