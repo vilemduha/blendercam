f85b8ac588d91c7b34d29d01e280bed2548322c7
vilda.novak@gmail.com <vilda.novak@gmail.com@d56605d3-40e8-9911-096e-8b66a8177f32>
2013-05-26 14:52:38 +0000

    adding presets. most of them were provided by Bernhard.

---
+++ scripts/presets/cam_machines/emc_test_2.py
@@ -0,0 +1,16 @@
+import bpy
+d = bpy.context.scene.cam_machine[0]
+s = bpy.context.scene.unit_settings
+
+d.exporter = 'EMC'
+s.system = 'IMPERIAL'
+d.working_area = (0.800000011920929, 0.5600000023841858, 0.09000000357627869)
+d.feedrate_min = 9.999999747378752e-06
+d.feedrate_max = 2.0
+d.feedrate_default = 1.5
+d.spindle_min = 5000.0
+d.spindle_max = 25000.0
+d.spindle_default = 20000.0
+d.axis4 = False
+d.axis5 = False
+d.collet_size = 0.0
