4ae5e4b118a6ba3bf82b69e670dcaa694c2703d2
vilda.novak@gmail.com <vilda.novak@gmail.com@d56605d3-40e8-9911-096e-8b66a8177f32>
2013-11-24 01:06:12 +0000

    comment

--- scripts/addons/cam/collision.py
+++ scripts/addons/cam/collision.py
@@ -76,7 +76,7 @@ def prepareBulletCollision(o):
 		collisionob=bpy.context.active_object
 		bpy.ops.rigidbody.object_add(type='ACTIVE')#using active instead of passive because of performance.TODO: check if this works also with 4axis...
 		collisionob.rigid_body.collision_shape = 'MESH'
-		collisionob.rigid_body.kinematic=True#268 and 70 
+		collisionob.rigid_body.kinematic=True#this fixed a serious bug and gave big speedup, rbs could move since they are now active...
 		collisionob.rigid_body.collision_margin = o.skin*BULLET_SCALE
 		bpy.ops.transform.resize(value=(BULLET_SCALE, BULLET_SCALE, BULLET_SCALE), constraint_axis=(False, False, False), constraint_orientation='GLOBAL', mirror=False, proportional='DISABLED', proportional_edit_falloff='SMOOTH', proportional_size=1, snap=False, snap_target='CLOSEST', snap_point=(0, 0, 0), snap_align=False, snap_normal=(0, 0, 0), texture_space=False, release_confirm=False)
 		collisionob.location=collisionob.location*BULLET_SCALE
