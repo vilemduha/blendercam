abd97a8026c632299e6011ed3ecf2fe67bb394bb
vilda.novak@gmail.com <vilda.novak@gmail.com@d56605d3-40e8-9911-096e-8b66a8177f32>
2015-01-03 14:13:06 +0000

    fix a bug with units reporting when carving images, also fixed the same for reporting of chipload. slimmer gravos postprocessor, reads faster into their Armote software.

--- scripts/addons/cam/nc/gravos.py
+++ scripts/addons/cam/nc/gravos.py
@@ -18,7 +18,8 @@ class Creator(iso.Creator):
 		self.write( (';' + comment  + '\n') )
 	
 	def write_blocknum(self):
-		self.write(self.BLOCK() % self.n)
+		#optimise
+		#self.write(self.BLOCK() % self.n)
 		self.n += 1
 	
 		
@@ -26,7 +27,9 @@ class Creator(iso.Creator):
 	def FORMAT_DWELL(self): return( self.SPACE() + self.DWELL() + ' X%f')
 	
 	def SPINDLE_OFF(self): return('M05')
-	
+	#optimize
+	def RAPID(self): return('G0')
+	def FEED(self): return('G1')
 	'''
 	def SPINDLE_DWELL(self,dwell):
 		w='\n'+self.BLOCK() % self.n+ self.DWELL() % dwell
@@ -57,10 +60,13 @@ class Creator(iso.Creator):
 	#	if self.s.str!=None:
 	#		self.write(self.s.str)
 	#	self.s.str = None
-		
+	
+	def PROGRAM_END(self): return( 'M30')
+	
 	def program_end(self):
 		self.write_blocknum()
 		self.write(self.SPACE() + self.SPINDLE_OFF() + self.SPACE() + self.PROGRAM_END() + '\n')
+		
 
 nc.creator = Creator()
 
