11c546eff57f9f61398b26ce8cc562eddb92d9a1
vilda.novak@gmail.com <vilda.novak@gmail.com@d56605d3-40e8-9911-096e-8b66a8177f32>
2015-03-03 19:16:07 +0000

    -fix 4 axis patterns and cutter rotations, it was all wrong... dunno when that happened, first tests worked nicely some time ago, must have done some weird reshuffling...

--- trunk/scripts/addons/cam/pattern.py
+++ trunk/scripts/addons/cam/pattern.py
@@ -398,6 +398,7 @@ def getPathPattern4axis(operation):
 		a2=0
 		a3=1
 	
+	o.max.z=o.maxz
 	#set radius for all types of operation
 	m1=max(abs(o.min[a2]),abs(o.max[a2]))
 	m2=max(abs(o.min[a3]),abs(o.max[a3]))
@@ -431,21 +432,24 @@ def getPathPattern4axis(operation):
 			cutterstart[a1]=o.min[a1]+a*o.dist_between_paths
 			cutterend[a1]=cutterstart[a1]
 			
-			cutterstart[a2]=radius
-			cutterend[a2]=radiusend
+			cutterstart[a2]=0#radius
+			cutterend[a2]=0#radiusend
+			
+			cutterstart[a3]=radius
+			cutterend[a3]=radiusend
 			
 			for b in range(0,floor(circlesteps)+1):
 				#print(cutterstart,cutterend)
 				chunk.startpoints.append(cutterstart.to_tuple())
 				chunk.endpoints.append(cutterend.to_tuple())
 				rot=[0,0,0]
-				rot[a1]=b*anglestep
+				rot[a1]=a*2*pi+b*anglestep
 				
 				chunk.rotations.append(rot)
 				cutterstart.rotate(e)
 				cutterend.rotate(e)
 
-			chunk.depth=-radius
+			chunk.depth=radiusend-radius
 			#last point = first
 			chunk.startpoints.append(chunk.startpoints[0])
 			chunk.endpoints.append(chunk.endpoints[0])
@@ -460,8 +464,10 @@ def getPathPattern4axis(operation):
 		for b in range(0,floor(circlesteps)+1):
 			chunk=camPathChunk([])
 			cutterstart[a2]=0
-			cutterstart[a2]=radius
-			cutterend[a2]=radiusend
+			cutterstart[a3]=radius
+			
+			cutterend[a2]=0
+			cutterend[a3]=radiusend
 			
 			e[a1]=anglestep*b
 			
@@ -477,7 +483,7 @@ def getPathPattern4axis(operation):
 				rot[a1]=b*anglestep
 				chunk.rotations.append(rot)
 				
-			chunk.depth=-radius
+			chunk.depth=radiusend-radius
 			#last point = first
 
 			pathchunks.append(chunk)
@@ -510,13 +516,13 @@ def getPathPattern4axis(operation):
 				chunk.endpoints.append(cutterend.to_tuple())
 				
 				rot=[0,0,0]
-				rot[a1]=b*anglestep
+				rot[a1]=a*2*pi+b*anglestep
 				chunk.rotations.append(rot)
 				
 				cutterstart.rotate(e)
 				cutterend.rotate(e)
 
-			chunk.depth=-radius
+			chunk.depth=radiusend-radius
 			#last point = first
 			
 	
