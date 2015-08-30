8e25fe4e26a2255279741f2dab12caee53018f00
Jeff Doyle (nfz) <shelleydoyle@eastlink.ca>
2015-08-29 23:19:59 -0300

    give default Feedrate a unit of length - easier for user

--- scripts/addons/cam/__init__.py
+++ scripts/addons/cam/__init__.py
@@ -98,7 +98,7 @@ class machineSettings(bpy.types.PropertyGroup):
 	working_area=bpy.props.FloatVectorProperty(name = 'Work Area', default=(0.500,0.500,0.100), unit='LENGTH', precision=PRECISION,subtype="XYZ",update = updateMachine)
 	feedrate_min=bpy.props.FloatProperty(name="Feedrate minimum /min", default=0.0, min=0.00001, max=320000,precision=PRECISION, unit='LENGTH')
 	feedrate_max=bpy.props.FloatProperty(name="Feedrate maximum /min", default=2, min=0.00001, max=320000,precision=PRECISION, unit='LENGTH')
-	feedrate_default=bpy.props.FloatProperty(name="Feedrate default /min", default=1.5, min=0.00001, max=320000,precision=PRECISION)
+	feedrate_default=bpy.props.FloatProperty(name="Feedrate default /min", default=1.5, min=0.00001, max=320000,precision=PRECISION, unit='LENGTH')
 	#UNSUPPORTED:
 	spindle_min=bpy.props.FloatProperty(name="#Spindlespeed minimum /min", default=5000, min=0.00001, max=320000,precision=1)
 	spindle_max=bpy.props.FloatProperty(name="#Spindlespeed maximum /min", default=30000, min=0.00001, max=320000,precision=1)
@@ -880,4 +880,3 @@ def unregister():
 
 if __name__ == "__main__":
 	register()
-	
