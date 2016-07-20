3a04432f647a4796fcf1efaa895b4652466d7c7f
TurBoss <j.l.toledano.l@gmail.com>
2016-07-20 20:38:10 +0200

    %

--- scripts/addons/cam/nc/fadal.py
+++ scripts/addons/cam/nc/fadal.py
@@ -5,6 +5,7 @@
 #
 # TurBoss, 19/06/2016
 
+import math
 from . import nc
 from . import iso
 from .format import Format
@@ -14,7 +15,30 @@ class Creator(iso.Creator):
 	def __init__(self): 
 		iso.Creator.__init__(self) 
 		
+		# internal variables
+		
 		self.fmt = Format(add_trailing_zeros = True)
-
+		self.output_block_numbers = True
+		self.start_block_number = 0
+		self.block_number_increment = 1
+		
+	############################################################################
+	##  Codes
+	
 	def SPACE_STR(self): return ' '
+		  
+	############################################################################
+	##  Programs
+
+	def program_begin(self, id, name=''):
+		if self.use_this_program_id:
+			id = self.use_this_program_id
+		if self.PROGRAM() != None:
+			self.write('%')
+			self.write('\n')
+			self.writem([(self.PROGRAM() % id), self.SPACE(), (self.COMMENT(name))])
+			self.write('\n')
+		self.program_id = id
+		self.program_name = name
+
 nc.creator = Creator()
