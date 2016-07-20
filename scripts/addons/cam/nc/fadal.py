71ebd621184a1d472c62efcbb388f9fd2751929d
TurBoss <j.l.toledano.l@gmail.com>
2016-07-20 22:31:43 +0200

    add "%" at the end of the file

--- scripts/addons/cam/nc/fadal.py
+++ scripts/addons/cam/nc/fadal.py
@@ -42,5 +42,28 @@ class Creator(iso.Creator):
 			self.write('\n')
 		self.program_id = id
 		self.program_name = name
+		
+	def program_end(self):
+		if self.z_for_g53 != None:
+			self.write(self.SPACE() + self.MACHINE_COORDINATES() + self.SPACE() + 'Z' + self.fmt.string(self.z_for_g53) + '\n')
+		self.write(self.SPACE() + self.PROGRAM_END() + '\n')
+		self.write('%')
+
+		if self.temp_file_to_append_on_close != None:
+			f_in = open(self.temp_file_to_append_on_close, 'r')
+			while (True):
+				line = f_in.readline()
+				if (len(line) == 0) : break
+				self.write(line)
+			f_in.close()
+			
+		self.file_close()
+			
+		if self.output_block_numbers:
+			# number every line of the file afterwards
+			self.number_file(self.filename)
+			
+			for f in self.subroutine_files:
+				self.number_file(f)
 
 nc.creator = Creator()
