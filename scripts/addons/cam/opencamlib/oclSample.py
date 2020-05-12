ba6652a0c92b33c0d747b0d392ab1adffc1c0af8
ksuprynowicz <ksuprynowicz@post.pl>
2020-05-12 21:50:55 +0200

    Fixed Parallel operation with OpenCAMLib

--- scripts/addons/cam/opencamlib/oclSample.py
+++ scripts/addons/cam/opencamlib/oclSample.py
@@ -3,74 +3,39 @@ import ocl
 import tempfile
 import camvtk
 import bpy
-
-from .VTKBlender import BlenderToPolyData
+from io_mesh_stl import blender_utils
+import mathutils
 from cam.simple import activate
 
-OCL_SCALE = 1000
+OCL_SCALE = 1000.0
 
-
-# ---- misc helper functions
-def vtkPolyData2OCLSTL(vtkPolyData, oclSTL):
-    """ read vtkPolyData and add each triangle to an ocl.STLSurf """
-    i = 0
-    for cellId in range(0, vtkPolyData.GetNumberOfCells()):
-        cell = vtkPolyData.GetCell(cellId)
-        points = cell.GetPoints()
-        plist = [ocl.Point(0.0, 0.0, 0.0),
-                 ocl.Point(0.0, 0.0, 0.0),
-                 ocl.Point(0.0, 0.0, 0.0)]
-        for pointId in range(0, points.GetNumberOfPoints()):
-            vertex = points.GetPoint(pointId)
-            p = ocl.Point(vertex[0], vertex[1], vertex[2])
-            plist[pointId] = p
-        i += 1
-        t = ocl.Triangle(plist[0], plist[1], plist[2])
-        oclSTL.addTriangle(t)
-
-
-def get_mesh(operation):
+def get_oclSTL(operation):
     me = None
+    oclSTL = ocl.STLSurf()
 
     for collision_object in operation.objects:
         activate(collision_object)
-        bpy.ops.object.duplicate(linked=False)
-        # bpy.ops.object.transform_apply(location=True, rotation=True, scale=True)
         if collision_object.type == "MESH":
-            bpy.ops.transform.resize(value=(OCL_SCALE, OCL_SCALE, OCL_SCALE), constraint_axis=(False, False, False),
-                                     orient_type='GLOBAL', mirror=False, use_proportional_edit=False,
-                                     proportional_edit_falloff='SMOOTH', proportional_size=1, snap=False,
-                                     snap_target='CLOSEST', snap_point=(0, 0, 0), snap_align=False,
-                                     snap_normal=(0, 0, 0),
-                                     texture_space=False, release_confirm=False)
-            bpy.ops.object.transform_apply(location=True, rotation=True, scale=True)
-            me = collision_object.data
-        bpy.ops.object.delete()
+            global_matrix = mathutils.Matrix.Identity(4)
+            faces = blender_utils.faces_from_mesh(collision_object, global_matrix, operation.use_modifiers)
+            for face in faces:
+                t = ocl.Triangle(ocl.Point(face[0][0]*OCL_SCALE, face[0][1]*OCL_SCALE, face[0][2]*OCL_SCALE),
+                        ocl.Point(face[1][0]*OCL_SCALE, face[1][1]*OCL_SCALE, face[1][2]*OCL_SCALE),
+                        ocl.Point(face[2][0]*OCL_SCALE, face[2][1]*OCL_SCALE, face[2][2]*OCL_SCALE))
 
         # FIXME needs to work with collections
-    return me
+    return oclSTL
 
 
 def ocl_sample(operation, chunks):
 
-    me = get_mesh(operation)
-    b2pd = BlenderToPolyData.convert(me=me)
-
-    stl_surf = ocl.STLSurf()
-    vtkPolyData2OCLSTL(b2pd, stl_surf)
+    oclSTL = get_oclSTL(operation)
 
     op_cutter_type = operation.cutter_type
     op_cutter_diameter = operation.cutter_diameter
     op_minz = operation.minz
     if op_cutter_type == "VCARVE":
         op_cutter_tip_angle = operation['cutter_tip_angle']
-
-    # with open(os.path.join(tempfile.gettempdir(), 'ocl_settings.txt'), 'r') as csv_file:
-    #     op_cutter_type = csv_file.readline().split()[0]
-    #     op_cutter_diameter = float(csv_file.readline())
-    #     if op_cutter_type == "VCARVE":
-    #         op_cutter_tip_angle = float(csv_file.readline())
-    #     op_minz = float(csv_file.readline())
 
     cutter = None
     cutter_length = 5
@@ -87,7 +52,7 @@ def ocl_sample(operation, chunks):
 
     # add BullCutter
     bdc = ocl.BatchDropCutter()
-    bdc.setSTL(stl_surf)
+    bdc.setSTL(oclSTL)
     bdc.setCutter(cutter)
 
     for chunk in chunks:
@@ -97,9 +62,5 @@ def ocl_sample(operation, chunks):
     bdc.run()
 
     cl_points = bdc.getCLPoints()
-
-    # with open(os.path.join(tempfile.gettempdir(), 'ocl_chunk_samples.txt'), 'w') as csv_file:
-    #     for point in cl_points:
-    #         csv_file.write(str(point.z) + '\n')
 
     return cl_points
