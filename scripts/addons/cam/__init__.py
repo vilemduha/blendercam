c657942508b9fcba0ba53b3298286ca0e243e455
Alain Pelletier <33393909+pppalain@users.noreply.github.com>
2021-02-13 15:56:24 -0400

    Update __init__.py
    house keeping

--- scripts/addons/cam/__init__.py
+++ scripts/addons/cam/__init__.py
@@ -400,18 +400,19 @@ def getStrategyList(scene, context):
         ('CIRCLES', 'Circles', 'Circles path'),
         ('OUTLINEFILL', 'Outline Fill',
          'Detect outline and fill it with paths as pocket. Then sample these paths on the 3d surface'),
-        ('CARVE', 'Carve', 'Pocket operation')
+        ('CARVE', 'Carve', 'Pocket operation'),
+        ('WATERLINE', 'Waterline - Roughing -below zero', 'Waterline paths - constant z below zero')
+        
     ]
     if use_experimental:
-        items.extend([('WATERLINE', 'Waterline - Roughing -below zero', 'Waterline paths - constant z below zero'),
-                      ('CURVE', 'Curve to Path - EXPERIMENTAL', 'Curve object gets converted directly to path'),
-                      ('PENCIL', 'Pencil - EXPERIMENTAL',
-                       'Pencil operation - detects negative corners in the model and mills only those.'),
-                      ('CRAZY', 'Crazy path - EXPERIMENTAL', 'Crazy paths - dont even think about using this!'),
+        items.extend([('CURVE', 'Curve to Path - EXPERIMENTAL', 'Curve object gets converted directly to path'),
                       ('MEDIAL_AXIS', 'Medial axis - EXPERIMENTAL',
                        'Medial axis, must be used with V or ball cutter, for engraving various width shapes with a single stroke '),
                       ('PROJECTED_CURVE', 'Projected curve - EXPERIMENTAL', 'project 1 curve towards other curve')])
     return items
+
+#('PENCIL', 'Pencil - EXPERIMENTAL','Pencil operation - detects negative corners in the model and mills only those.'),
+#('CRAZY', 'Crazy path - EXPERIMENTAL', 'Crazy paths - dont even think about using this!'),
 
 
 class camOperation(bpy.types.PropertyGroup):
