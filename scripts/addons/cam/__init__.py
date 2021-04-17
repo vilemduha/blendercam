b83896742c6eafdc212cdbc6970cf3062abba74a
Alain Pelletier <33393909+pppalain@users.noreply.github.com>
2021-04-17 15:12:44 -0300

    curve to path
    curve to path no longer experimental

--- scripts/addons/cam/__init__.py
+++ scripts/addons/cam/__init__.py
@@ -402,12 +402,11 @@ def getStrategyList(scene, context):
         ('OUTLINEFILL', 'Outline Fill',
          'Detect outline and fill it with paths as pocket. Then sample these paths on the 3d surface'),
         ('CARVE', 'Project curve to surface', 'Engrave the curve path to surface'),
-        ('WATERLINE', 'Waterline - Roughing -below zero', 'Waterline paths - constant z below zero')
-        
+        ('WATERLINE', 'Waterline - Roughing -below zero', 'Waterline paths - constant z below zero'),
+        ('CURVE', 'Curve to Path', 'Curve object gets converted directly to path')
     ]
     if use_experimental:
-        items.extend([('CURVE', 'Curve to Path - EXPERIMENTAL', 'Curve object gets converted directly to path'),
-                      ('MEDIAL_AXIS', 'Medial axis - EXPERIMENTAL',
+        items.extend([('MEDIAL_AXIS', 'Medial axis - EXPERIMENTAL',
                        'Medial axis, must be used with V or ball cutter, for engraving various width shapes with a single stroke '),
                       ('PROJECTED_CURVE', 'Projected curve - EXPERIMENTAL', 'project 1 curve towards other curve')])
     return items
