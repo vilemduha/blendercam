5629fc6b9bc7cdcfd51970f1a95d9a67aacdbed7
palain <bereflective@gmail.com>
2021-08-12 09:13:01 -0300

    done testing

--- scripts/addons/cam/curvecamtools.py
+++ scripts/addons/cam/curvecamtools.py
@@ -20,7 +20,7 @@
 # ***** END GPL LICENCE BLOCK *****
 
 # blender operators definitions are in this file. They mostly call the functions from utils.py
-#test3
+
 
 import bpy
 from bpy.props import *
