2b2d3a309b9751896c0f51de61b14c561ff7944c
migo101 <migo101@gmx.fr>
2022-03-22 11:00:21 +0100

    More reformatting of CAM_INFO_Panel

--- scripts/addons/cam/ui.py
+++ scripts/addons/cam/ui.py
@@ -33,6 +33,8 @@ from bpy.types import (Panel, Menu, Operator, PropertyGroup, )
 from cam import gcodeimportparser, simple
 from cam.simple import *
 
+import ocl
+
 
 # EXPERIMENTAL=True#False
 
@@ -432,16 +434,15 @@ class CAM_INFO_Panel(CAMButtonsPanel, bpy.types.Panel):
 
     def draw_opencamlib_version(self):
         if "ocl" in sys.modules:
-            #TODO: Display Opencamlib's version
-            self.layout.label(text = "Opencamlib installed")
+            self.layout.label(text = "Opencamlib v%s installed" % ocl.version())
         else:
             self.layout.label(text = "Opencamlib is not installed")
 
     def draw_active_op_warnings(self):
         active_op = self.scene.cam_operations[self.scene.cam_active_operation]
         if active_op.warnings != '':
-            for line in active_op.warnings.split('\n'):
-                self.layout.label(text=line, icon='COLOR_RED')
+            for line in active_op.warnings.rstrip("\n").split("\n"):
+                self.layout.label(text=line, icon='ERROR')
 
     def draw_active_op_data(self):
         active_op = self.scene.cam_operations[self.scene.cam_active_operation]
@@ -450,9 +451,9 @@ class CAM_INFO_Panel(CAMButtonsPanel, bpy.types.Panel):
 
         active_op_time_text = "Operation Time: %d s " % int(active_op.duration*60)
         if active_op.duration > 60:
-            active_op_time_text += " (%d h %d min)" % (int(active_op.duration / 60), round(active_op.duration % 60))
+            active_op_time_text += " (%dh %dmin)" % (int(active_op.duration / 60), round(active_op.duration % 60))
         elif active_op.duration > 1:
-            active_op_time_text += " (%d min)" % round(active_op.duration % 60)
+            active_op_time_text += " (%dmin)" % round(active_op.duration % 60)
 
         self.layout.label(text = active_op_time_text)
 
@@ -464,14 +465,16 @@ class CAM_INFO_Panel(CAMButtonsPanel, bpy.types.Panel):
         if not active_op.valid: return
         if not int(active_op.duration*60) > 0: return
 
+        # TODO: the hourly_rate button properties should be moved here (UI related only)
+        # Right now, trying to do so causes an error
         self.layout.prop(self.scene.cam_machine, 'hourly_rate')
+
         if float(self.scene.cam_machine.hourly_rate) < 0.01: return
 
         cost_per_second = self.scene.cam_machine.hourly_rate / 3600
-        active_op_cost = 'Operation cost: $' + str(round((active_op.duration * 60 * cost_per_second), 2))
-        self.layout.label(text='Cost per second:' + str(round(cost_per_second, 3)))
-        self.layout.label(text=active_op_cost)
-
+        self.layout.label(text = "Operation cost: $%.2f (%.2f $/s)"
+            % (active_op.duration * 60 * cost_per_second, cost_per_second)
+        )
 
 
 
