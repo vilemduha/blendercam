c17c2cc6aa95a3cabebc55a90050c12cd6482fd7
palain <bereflective@gmail.com>
2023-04-03 14:52:00 -0300

    pocket closing for center - fixes rectangle pockets

--- scripts/addons/cam/strategy.py
+++ scripts/addons/cam/strategy.py
@@ -329,7 +329,10 @@ def pocket(o):
 
         nchunks = shapelyToChunks(p, o.min.z)
         # print("nchunks")
-        pnew = p.buffer(-o.dist_between_paths, o.circle_detail)
+        endDiv = 2
+        if i < approxn -2:     #detect end of rectangular pocket
+            endDiv = 1
+        pnew = p.buffer(-o.dist_between_paths/endDiv, o.circle_detail)
         # print("pnew")
 
         nchunks = limitChunks(nchunks, o)
