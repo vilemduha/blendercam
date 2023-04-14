929d5ea3730d3387389669f0ff74cc830d09d772
palain <bereflective@gmail.com>
2023-04-14 11:26:18 -0300

    subdivide crosshatch curves.

--- scripts/addons/cam/curvecamcreate.py
+++ scripts/addons/cam/curvecamcreate.py
@@ -108,6 +108,7 @@ class CamCurveHatch(bpy.types.Operator):
         bpy.ops.curve.subdivide()
         bpy.ops.object.editmode_toggle()
         simple.join_multiple('crosshatch')
+        simple.remove_doubles()
         return {'FINISHED'}
 
 
