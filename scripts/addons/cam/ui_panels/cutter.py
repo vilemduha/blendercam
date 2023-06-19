d026f671b7425e01ca6b2c2add02ed46319f85ed
migo101 <migo101@gmx.fr>
2023-06-19 16:07:57 +0200

    Added levels to cutter

--- scripts/addons/cam/ui_panels/cutter.py
+++ scripts/addons/cam/ui_panels/cutter.py
@@ -5,89 +5,134 @@ from cam.ui_panels.buttons_panel import CAMButtonsPanel
 
 class CAM_CUTTER_Panel(CAMButtonsPanel, bpy.types.Panel):
     """CAM cutter panel"""
-    bl_label = " "
+    bl_label = "CAM Cutter"
     bl_idname = "WORLD_PT_CAM_CUTTER"
     panel_interface_level = 0
 
-    COMPAT_ENGINES = {'BLENDERCAM_RENDER'}
+    prop_level = {
+        'preset_menu': 1,
+        'cutter_id': 2,
+        'cutter_type': 0,
+        'ball_radius': 0,
+        'bull_radius': 0,
+        'cylcone_diameter': 0,
+        'tip_angle': 0,
+        'laser': 0,
+        'plasma': 0,
+        'custom': 0,
+        'diameter': 0,
+        'flutes': 1,
+        'description': 1,
+        'engagement': 0
+    }
 
 
-    # Displays percentage of the cutter which is engaged with the material
-    # Displays a warning for engagements greater than 50%
-    def EngagementDisplay(self, operat, layout):
-        ao = operat
+    def draw_cutter_preset_menu(self):
+        if not self.has_correct_level('preset_menu'): return
+        row = self.layout.row(align=True)
+        row.menu("CAM_CUTTER_MT_presets", text=bpy.types.CAM_CUTTER_MT_presets.bl_label)
+        row.operator("render.cam_preset_cutter_add", text="", icon='ADD')
+        row.operator("render.cam_preset_cutter_add", text="", icon='REMOVE').remove_active = True
 
-        if ao.cutter_type == 'BALLCONE':
-            if ao.dist_between_paths > ao.ball_radius:
-                layout.label(text="CAUTION: CUTTER ENGAGEMENT")
-                layout.label(text="GREATER THAN 50%")
-            layout.label(text="Cutter engagement: " + str(round(100 * ao.dist_between_paths / ao.ball_radius, 1)) + "%")
+    def draw_cutter_id(self):
+        if not self.has_correct_level('cutter_id'): return
+        self.layout.prop(self.op, 'cutter_id')
+
+    def draw_cutter_type(self):
+        if not self.has_correct_level('cutter_type'): return
+        self.layout.prop(self.op, 'cutter_type')
+
+    def draw_ball_radius(self):
+        if not self.has_correct_level('ball_radius'): return
+        if self.op.cutter_type in ['BALLCONE']:
+            self.layout.prop(self.op, 'ball_radius')
+
+    def draw_bull_radius(self):
+        if not self.has_correct_level('bull_radius'): return
+        if self.op.cutter_type in ['BULLNOSE']:
+            self.layout.prop(self.op, 'bull_corner_radius')
+
+    def draw_cylcone_diameter(self):
+        if not self.has_correct_level('cylcone_diameter'): return
+        if self.op.cutter_type in ['CYLCONE']:
+            self.layout.prop(self.op, 'cylcone_diameter')
+
+    def draw_cutter_tip_angle(self):
+        if not self.has_correct_level('tip_angle'): return
+        if self.op.cutter_type in ['VCARVE', 'BALLCONE', 'BULLNOSE', 'CYLCONE']:
+            self.layout.prop(self.op, 'cutter_tip_angle')
+
+    def draw_laser(self):
+        if not self.has_correct_level('laser'): return
+        if self.op.cutter_type in ['LASER']:
+            self.layout.prop(self.op, 'Laser_on')
+            self.layout.prop(self.op, 'Laser_off')
+            self.layout.prop(self.op, 'Laser_cmd')
+            self.layout.prop(self.op, 'Laser_delay')
+
+    def draw_plasma(self):
+        if not self.has_correct_level('plasma'): return
+        if self.op.cutter_type in ['PLASMA']:
+            self.layout.prop(self.op, 'Plasma_on')
+            self.layout.prop(self.op, 'Plasma_off')
+            self.layout.prop(self.op, 'Plasma_delay')
+            self.layout.prop(self.op, 'Plasma_dwell')
+            self.layout.prop(self.op, 'lead_in')
+            self.layout.prop(self.op, 'lead_out')
+
+    def draw_custom(self):
+        if not self.has_correct_level('custom'): return
+        if self.op.cutter_type in ['CUSTOM']:
+            if self.op.optimisation.use_exact:
+                self.layout.label(text='Warning - only convex shapes are supported. ', icon='COLOR_RED')
+                self.layout.label(text='If your custom cutter is concave,')
+                self.layout.label(text='switch exact mode off.')
+            self.layout.prop_search(self.op, "cutter_object_name", bpy.data, "objects")
+
+    def draw_cutter_diameter(self):
+        if not self.has_correct_level('diameter'): return
+        self.layout.prop(self.op, 'cutter_diameter')
+
+    def draw_cutter_flutes(self):
+        if not self.has_correct_level('flutes'): return
+        if self.op.cutter_type not in ['LASER', 'PLASMA']:
+            self.layout.prop(self.op, 'cutter_flutes')
+
+    def draw_cutter_description(self):
+        if not self.has_correct_level('description'): return
+        self.layout.prop(self.op, 'cutter_description')
+
+    def draw_engagement(self):
+        if not self.has_correct_level('engagement'): return
+        if self.op.cutter_type in ['LASER', 'PLASMA']: return
+        if self.op.strategy in ['CUTOUT']: return
+
+        if self.op.cutter_type in ['BALLCONE']:
+            engagement = round(100 * self.op.dist_between_paths / self.op.ball_radius, 1)
         else:
-            if ao.dist_between_paths > ao.cutter_diameter / 2:
-                layout.label(text="CAUTION: CUTTER ENGAGEMENT")
-                layout.label(text="GREATER THAN 50%")
-            layout.label(text="Cutter Engagement: " + str(round(100 * ao.dist_between_paths / ao.cutter_diameter, 1)) + "%")
+            engagement = round(100 * self.op.dist_between_paths / self.op.cutter_diameter, 1)
 
-    def draw_header(self, context):
-        self.layout.menu("CAM_CUTTER_MT_presets", text="CAM Cutter")
+        self.layout.label(text=f"Cutter engagement: {engagement}%")
+
+        if engagement > 50:
+            self.layout.label(text="WARNING: CUTTER ENGAGEMENT > 50%")
 
     def draw(self, context):
-        layout = self.layout
-        d = bpy.context.scene
-        if len(d.cam_operations) > 0:
-            ao = d.cam_operations[d.cam_active_operation]
+        self.context = context
 
-            if ao:
-                # cutter preset
-                row = layout.row(align=True)
-                row.menu("CAM_CUTTER_MT_presets", text=bpy.types.CAM_CUTTER_MT_presets.bl_label)
-                row.operator("render.cam_preset_cutter_add", text="", icon='ADD')
-                row.operator("render.cam_preset_cutter_add", text="", icon='REMOVE').remove_active = True
-                layout.prop(ao, 'cutter_id')
-                layout.prop(ao, 'cutter_type')
-                if ao.cutter_type == 'VCARVE':
-                    layout.prop(ao, 'cutter_tip_angle')
-                if ao.cutter_type == 'BALLCONE':
-                    layout.prop(ao, 'ball_radius')
-                    self.EngagementDisplay(ao, layout)
-                    layout.prop(ao, 'cutter_tip_angle')
-                    layout.label(text='Cutter diameter = shank diameter')
-                if ao.cutter_type == 'CYLCONE':
-                    layout.prop(ao, 'cylcone_diameter')
-                    self.EngagementDisplay(ao, layout)
-                    layout.prop(ao, 'cutter_tip_angle')
-                    layout.label(text='Cutter diameter = shank diameter')
-                if ao.cutter_type == 'BULLNOSE':
-                    layout.prop(ao, 'bull_corner_radius')
-                    self.EngagementDisplay(ao, layout)
-                    layout.label(text='Cutter diameter = shank diameter')
+        self.draw_cutter_preset_menu()
+        self.draw_cutter_id()
+        self.draw_cutter_type()
+        self.draw_ball_radius()
+        self.draw_bull_radius()
+        self.draw_cylcone_diameter()
+        self.draw_cutter_tip_angle()
+        self.draw_laser()
+        self.draw_plasma()
+        self.draw_custom()
+        self.draw_cutter_diameter()
+        self.draw_cutter_flutes()
+        self.draw_cutter_description()
+        self.draw_engagement()
 
-                if ao.cutter_type == 'LASER':
-                    layout.prop(ao, 'Laser_on')
-                    layout.prop(ao, 'Laser_off')
-                    layout.prop(ao, 'Laser_cmd')
-                    layout.prop(ao, 'Laser_delay')
 
-                if ao.cutter_type == 'PLASMA':
-                    layout.prop(ao, 'Plasma_on')
-                    layout.prop(ao, 'Plasma_off')
-                    layout.prop(ao, 'Plasma_delay')
-                    layout.prop(ao, 'Plasma_dwell')
-                    layout.prop(ao, 'lead_in')
-                    layout.prop(ao, 'lead_out')
-
-                if ao.cutter_type == 'CUSTOM':
-                    if ao.optimisation.use_exact:
-                        layout.label(text='Warning - only convex shapes are supported. ', icon='COLOR_RED')
-                        layout.label(text='If your custom cutter is concave,')
-                        layout.label(text='switch exact mode off.')
-
-                    layout.prop_search(ao, "cutter_object_name", bpy.data, "objects")
-
-                layout.prop(ao, 'cutter_diameter')
-                if ao.strategy == "POCKET" or ao.strategy == "PARALLEL" or ao.strategy == "CROSS" \
-                        or ao.strategy == "WATERLINE":
-                    self.EngagementDisplay(ao, layout)
-                if ao.cutter_type != "LASER":
-                    layout.prop(ao, 'cutter_flutes')
-                layout.prop(ao, 'cutter_description')
