776ae20bc678f7cfbb705a08751b83fa2e0fa021
migo101 <migo101@gmx.fr>
2023-06-20 11:38:22 +0200

    Added levels for feedrate

--- scripts/addons/cam/ui_panels/feedrate.py
+++ scripts/addons/cam/ui_panels/feedrate.py
@@ -7,12 +7,39 @@ class CAM_FEEDRATE_Panel(CAMButtonsPanel, bpy.types.Panel):
     bl_idname = "WORLD_PT_CAM_FEEDRATE"
     panel_interface_level = 0
 
-    COMPAT_ENGINES = {'BLENDERCAM_RENDER'}
+    prop_level = {
+        'feedrate': 0,
+        'sim_feedrate': 2,
+        'plunge_feedrate': 1,
+        'plunge_angle': 1,
+        'spindle_rpm': 0
+    }
+
+    def draw_feedrate(self):
+        if not self.has_correct_level('feedrate'): return
+        self.layout.prop(self.op, 'feedrate')
+
+    def draw_sim_feedrate(self):
+        if not self.has_correct_level('sim_feedrate'): return
+        self.layout.prop(self.op, 'do_simulation_feedrate')
+
+    def draw_plunge_feedrate(self):
+        if not self.has_correct_level('plunge_feedrate'): return
+        self.layout.prop(self.op, 'plunge_feedrate')
+
+    def draw_plunge_angle(self):
+        if not self.has_correct_level('plunge_angle'): return
+        self.layout.prop(self.op, 'plunge_angle')
+
+    def draw_spindle_rpm(self):
+        if not self.has_correct_level('spindle_rpm'): return
+        self.layout.prop(self.op, 'spindle_rpm')
 
     def draw(self, context):
+        self.context = context
 
-        self.layout.prop(ao, 'feedrate')
-        self.layout.prop(ao, 'do_simulation_feedrate')
-        self.layout.prop(ao, 'plunge_feedrate')
-        self.layout.prop(ao, 'plunge_angle')
-        self.layout.prop(ao, 'spindle_rpm')
+        self.draw_feedrate()
+        self.draw_sim_feedrate()
+        self.draw_plunge_feedrate()
+        self.draw_plunge_angle()
+        self.draw_spindle_rpm()
