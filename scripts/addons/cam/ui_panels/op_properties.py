8bf512afc30633342891821f6f881754951890ac
migo101 <migo101@gmx.fr>
2023-07-05 16:23:56 +0200

    Added strategies to levels

--- scripts/addons/cam/ui_panels/op_properties.py
+++ scripts/addons/cam/ui_panels/op_properties.py
@@ -12,9 +12,24 @@ class CAM_OPERATION_PROPERTIES_Panel(CAMButtonsPanel, bpy.types.Panel):
     prop_level = {
         'draw_cutter_engagement': 0,
         'draw_machine_axis': 2,
-        'draw_strategy': 0
+        'draw_strategy': 0,
+        'draw_enable_A_B_axis': 1,
+        'draw_cutout_options': 0,
+        'draw_waterline_options': 0,
+        'draw_medial_axis_options': 0,
+        'draw_drill_options': 0,
+        'draw_pocket_options': 0,
+        'draw_default_options': 0,
+        'draw_bridges_options': 1,
+        'draw_skin': 1,
+        'draw_array': 1,
+        'draw_cutout_type': 0,
+        'draw_overshoot': 1,
+        'draw_startpoint': 1,
+        'draw_lead_in_out': 1,
+        'draw_outlines': 2,
+        'draw_merge': 2
     }
-
 
     # Displays percentage of the cutter which is engaged with the material
     # Displays a warning for engagements greater than 50%
@@ -51,133 +66,137 @@ class CAM_OPERATION_PROPERTIES_Panel(CAMButtonsPanel, bpy.types.Panel):
         else:
             self.layout.prop(self.op, 'strategy')
 
+    def draw_enable_A_B_axis(self):
+        if not self.has_correct_level(): return
+        self.layout.prop(self.op, 'enable_A')
+        if self.op.enable_A:
+            self.layout.prop(self.op, 'rotation_A')
+            self.layout.prop(self.op, 'A_along_x')
+            if self.op.A_along_x:
+                self.layout.label(text='A || X - B || Y')
+            else:
+                self.layout.label(text='A || Y - B ||X')
 
-    def draw(self, context):
-        self.context = context
-
-        self.draw_machine_axis()
-        self.draw_strategy()
+        self.layout.prop(self.op, 'enable_B')
+        if self.op.enable_B:
+            self.layout.prop(self.op, 'rotation_B')
 
 
+    def draw_cutout_type(self):
+        if not self.has_correct_level(): return
+        self.layout.prop(self.op, 'cut_type')
 
+    def draw_overshoot(self):
+        if not self.has_correct_level(): return
+        self.layout.prop(self.op, 'straight')
 
-        if self.op.strategy in ['BLOCK', 'SPIRAL', 'CIRCLES', 'OUTLINEFILL']:
+    def draw_startpoint(self):
+        if not self.has_correct_level(): return
+        self.layout.prop(self.op, 'profile_start')
+
+    def draw_lead_in_out(self):
+        if not self.has_correct_level(): return
+        self.layout.prop(self.op, 'lead_in')
+        self.layout.prop(self.op, 'lead_out')
+
+    def draw_outlines(self):
+        if not self.has_correct_level(): return
+        self.layout.prop(self.op, 'outlines_count')
+        if self.op.outlines_count > 1:
+            self.layout.prop(self.op, 'dist_between_paths')
+            self.draw_cutter_engagement()
             self.layout.prop(self.op.movement, 'insideout')
 
+    def draw_merge(self):
+        if not self.has_correct_level(): return
+        self.layout.prop(self.op, 'dont_merge')
+
+    def draw_cutout_options(self):
+        if not self.has_correct_level(): return
+        if self.op.strategy in ['CUTOUT']:
+            self.draw_cutout_type()
+            self.draw_overshoot()
+            self.draw_startpoint()
+            self.draw_lead_in_out()
+
         if self.op.strategy in ['CUTOUT', 'CURVE']:
-            if self.op.strategy == 'CUTOUT':
-                self.layout.prop(self.op, 'cut_type')
-                self.layout.label(text="Overshoot works best with curve")
-                self.layout.label(text="having C remove doubles")
-                self.layout.prop(self.op, 'straight')
-                self.layout.prop(self.op, 'profile_start')
-                self.layout.label(text="Lead in / out not fully working")
-                self.layout.prop(self.op, 'lead_in')
-                self.layout.prop(self.op, 'lead_out')
+            self.draw_enable_A_B_axis()
+            self.draw_outlines()
+            self.draw_merge()
 
-            self.layout.prop(self.op, 'enable_A')
-            if self.op.enable_A:
-                self.layout.prop(self.op, 'rotation_A')
-                self.layout.prop(self.op, 'A_along_x')
-                if self.op.A_along_x:
-                    self.layout.label(text='A || X - B || Y')
-                else:
-                    self.layout.label(text='A || Y - B ||X')
 
-            self.layout.prop(self.op, 'enable_B')
-            if self.op.enable_B:
-                self.layout.prop(self.op, 'rotation_B')
-
-            self.layout.prop(self.op, 'outlines_count')
-            if self.op.outlines_count > 1:
-                self.layout.prop(self.op, 'dist_between_paths')
-                self.draw_cutter_engagement()
-                self.layout.prop(self.op.movement, 'insideout')
-            self.layout.prop(self.op, 'dont_merge')
-
-        elif self.op.strategy == 'WATERLINE':
+    def draw_waterline_options(self):
+        if not self.has_correct_level(): return
+        if self.op.strategy in ['WATERLINE']:
             self.layout.label(text="OCL doesn't support fill areas")
             if not self.op.optimisation.use_opencamlib:
                 self.layout.prop(self.op, 'slice_detail')
                 self.layout.prop(self.op, 'waterline_fill')
                 if self.op.waterline_fill:
                     self.layout.prop(self.op, 'dist_between_paths')
-                    self.draw_cutter_engagement()
                     self.layout.prop(self.op, 'waterline_project')
+            self.layout.label(text="Waterline needs a skin margin")
 
-        elif self.op.strategy == 'CARVE':
+    def draw_carve_options(self):
+        if not self.has_correct_level(): return
+        if self.op.strategy in ['CARVE']:
             self.layout.prop(self.op, 'carve_depth')
             self.layout.prop(self.op, 'dist_along_paths')
-        elif self.op.strategy == 'MEDIAL_AXIS':
+
+    def draw_medial_axis_options(self):
+        if not self.has_correct_level(): return
+        if self.op.strategy in ['MEDIAL_AXIS']:
             self.layout.prop(self.op, 'medial_axis_threshold')
             self.layout.prop(self.op, 'medial_axis_subdivision')
             self.layout.prop(self.op, 'add_pocket_for_medial')
             self.layout.prop(self.op, 'add_mesh_for_medial')
-        elif self.op.strategy == 'DRILL':
-            self.layout.prop(self.op, 'drill_type')
-            self.layout.prop(self.op, 'enable_A')
-            if self.op.enable_A:
-                self.layout.prop(self.op, 'rotation_A')
-                self.layout.prop(self.op, 'A_along_x')
-                if self.op.A_along_x:
-                    self.layout.label(text='A || X - B || Y')
-                else:
-                    self.layout.label(text='A || Y - B ||X')
-            self.layout.prop(self.op, 'enable_B')
-            if self.op.enable_B:
-                self.layout.prop(self.op, 'rotation_B')
 
-        elif self.op.strategy == 'POCKET':
+
+    def draw_drill_options(self):
+        if not self.has_correct_level(): return
+        if self.op.strategy in ['DRILL']:
+            self.layout.prop(self.op, 'drill_type')
+            self.draw_enable_A_B_axis()
+
+
+    def draw_pocket_options(self):
+        if not self.has_correct_level(): return
+        if self.op.strategy in ['POCKET']:
             self.layout.prop(self.op, 'pocket_option')
             self.layout.prop(self.op, 'pocketToCurve')
             self.layout.prop(self.op, 'dist_between_paths')
             self.draw_cutter_engagement()
-            self.layout.prop(self.op, 'enable_A')
-            if self.op.enable_A:
-                self.layout.prop(self.op, 'rotation_A')
-                self.layout.prop(self.op, 'A_along_x')
-                if self.op.A_along_x:
-                    self.layout.label(text='A || X - B || Y')
-                else:
-                    self.layout.label(text='A || Y - B ||X')
-            self.layout.prop(self.op, 'enable_B')
-            if self.op.enable_B:
-                self.layout.prop(self.op, 'rotation_B')
-        else:
+            self.draw_enable_A_B_axis()
+
+
+    def draw_default_options(self):
+        if not self.has_correct_level(): return
+        if self.op.strategy not in ['CUTOUT', 'CURVE', 'WATERLINE', 'CARVE', 'MEDIAL_AXIS', 'DRILL', 'POCKET']:
             self.layout.prop(self.op, 'dist_between_paths')
             self.draw_cutter_engagement()
             self.layout.prop(self.op, 'dist_along_paths')
-            if self.op.strategy == 'PARALLEL' or self.op.strategy == 'CROSS':
+            if self.op.strategy in ['PARALLEL','CROSS']:
                 self.layout.prop(self.op, 'parallel_angle')
-                self.layout.prop(self.op, 'enable_A')
-            if self.op.enable_A:
-                self.layout.prop(self.op, 'rotation_A')
-                self.layout.prop(self.op, 'A_along_x')
-                if self.op.A_along_x:
-                    self.layout.label(text='A || X - B || Y')
-                else:
-                    self.layout.label(text='A || Y - B ||X')
-            self.layout.prop(self.op, 'enable_B')
-            if self.op.enable_B:
-                self.layout.prop(self.op, 'rotation_B')
-
+                self.draw_enable_A_B_axis()
             self.layout.prop(self.op, 'inverse')
 
+    def draw_bridges_options(self):
+        if not self.has_correct_level(): return
         if self.op.strategy not in ['POCKET', 'DRILL', 'CURVE', 'MEDIAL_AXIS']:
             self.layout.prop(self.op, 'use_bridges')
             if self.op.use_bridges:
                 self.layout.prop(self.op, 'bridges_width')
                 self.layout.prop(self.op, 'bridges_height')
-
                 self.layout.prop_search(self.op, "bridges_collection_name", bpy.data, "collections")
                 self.layout.prop(self.op, 'use_bridge_modifiers')
             self.layout.operator("scene.cam_bridges_add", text="Autogenerate bridges")
 
-        if self.op.strategy == 'WATERLINE':
-                self.layout.label(text="Waterline roughing strategy")
-                self.layout.label(text="needs a skin margin")
+    def draw_skin(self):
+        if not self.has_correct_level(): return
         self.layout.prop(self.op, 'skin')
 
+    def draw_array(self):
+        if not self.has_correct_level(): return
         if self.op.machine_axes == '3':
             self.layout.prop(self.op, 'array')
             if self.op.array:
@@ -185,3 +204,22 @@ class CAM_OPERATION_PROPERTIES_Panel(CAMButtonsPanel, bpy.types.Panel):
                 self.layout.prop(self.op, 'array_x_distance')
                 self.layout.prop(self.op, 'array_y_count')
                 self.layout.prop(self.op, 'array_y_distance')
+
+
+    def draw(self, context):
+        self.context = context
+
+        self.draw_machine_axis()
+        self.draw_strategy()
+
+        self.draw_cutout_options()
+        self.draw_waterline_options()
+        self.draw_carve_options()
+        self.draw_medial_axis_options()
+        self.draw_drill_options()
+        self.draw_pocket_options()
+        self.draw_default_options()
+
+        self.draw_bridges_options()
+        self.draw_skin()
+        self.draw_array()
