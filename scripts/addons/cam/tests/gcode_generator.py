6140a5096937b46ae04d2c686b0350d885022aa2
migo101 <migo101@gmx.fr>
2023-07-06 17:30:22 +0200

    Added test suite

---
+++ scripts/addons/cam/tests/gcode_generator.py
@@ -0,0 +1,14 @@
+import bpy
+
+# Get the scene
+s = bpy.context.scene
+
+for i, operation in enumerate(s.cam_operations):
+    # Set the active operation using the index
+    s.cam_active_operation = i
+
+    print(f"############ Generating {operation.name} #############")
+
+    # Run the calculate_cam_path() operator
+    bpy.ops.object.calculate_cam_path()
+
