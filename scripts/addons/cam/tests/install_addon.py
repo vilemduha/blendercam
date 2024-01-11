a4a86f1e56e6e006086bee6002c632d15604ab83
Joe Marshall <joe.marshall@nottingham.ac.uk>
2024-01-11 11:24:59 +0000

    addon installer for testing on github actions

---
+++ scripts/addons/cam/tests/install_addon.py
@@ -0,0 +1,19 @@
+import tempfile
+import sys
+import subprocess
+import pathlib
+
+INSTALL_CODE=f"""
+import bpy
+bpy.ops.wm.addon_install(filepath='{sys.argv[1]}')
+bpy.ops.wm.addon_enable(module='testaddon')
+bpy.ops.wm.save_userpref()
+"""
+
+with tempfile.TemporaryDirectory() as td:
+  file=pathlib.Path(td,"install.py")
+  file.write_text(INSTALL_CODE)
+  command = f'blender -b -P "{str(file)}"'
+  subprocess.run(command, shell=True, check=True)
+
+
