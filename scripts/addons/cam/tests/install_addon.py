f80676fdb7478efe0edc8db178c2fc67f988d7d9
Joe Marshall <joe.marshall@nottingham.ac.uk>
2024-01-13 08:30:34 +0000

    retry installation

--- scripts/addons/cam/tests/install_addon.py
+++ scripts/addons/cam/tests/install_addon.py
@@ -10,10 +10,18 @@ bpy.ops.preferences.addon_enable(module='cam')
 bpy.ops.wm.save_userpref()
 """
 
+NUM_RETRIES=10
+
 with tempfile.TemporaryDirectory() as td:
   file=pathlib.Path(td,"install.py")
   file.write_text(INSTALL_CODE)
   command = f'blender -b -P "{str(file)}"'
-  subprocess.run(command, shell=True, check=True)
+  # blender 4.0 installing addon crashes sometimes on mac github actions...
+  for x in range(NUM_RETRIES):
+    try:
+      subprocess.run(command, shell=True, check=True)
+      sys.exit(0)
+    except Exception as e:
+      print("Install addon failed, retrying:",e)
 
 
