dd220598ad9b30563fed9421360e9e851f329ceb
Release robot <release-robot@users.noreply.github.com>
2024-01-30 14:19:07 +0000

    Version number

--- scripts/addons/cam/__init__.py
+++ scripts/addons/cam/__init__.py
@@ -60,7 +60,7 @@ from cam.version import __version__
 bl_info = {
     "name": "CAM - gcode generation tools",
     "author": "Vilem Novak",
-    "version":(0,9,11),
+    "version":(1,0,0),
     "blender": (3, 6, 0),
     "location": "Properties > render",
     "description": "Generate machining paths for CNC",
