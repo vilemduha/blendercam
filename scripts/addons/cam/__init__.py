2a6961ebbfaa5d7eb81e2540b7a61114b6c022a5
Release robot <release-robot@users.noreply.github.com>
2024-03-26 12:56:35 +0000

    Version number

--- scripts/addons/cam/__init__.py
+++ scripts/addons/cam/__init__.py
@@ -100,7 +100,7 @@ from shapely import geometry as sgeometry  # noqa
 bl_info = {
     "name": "CAM - gcode generation tools",
     "author": "Vilem Novak & Contributors",
-    "version": (1, 0, 7),
+    "version":(1,0,8),
 
     "blender": (3, 6, 0),
     "location": "Properties > render",
