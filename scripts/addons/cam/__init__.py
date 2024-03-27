46b91b72cf05cfec95d9cd861401f05444da639f
Release robot <release-robot@users.noreply.github.com>
2024-03-27 17:34:48 +0000

    Version number

--- scripts/addons/cam/__init__.py
+++ scripts/addons/cam/__init__.py
@@ -100,7 +100,7 @@ from shapely import geometry as sgeometry  # noqa
 bl_info = {
     "name": "CAM - gcode generation tools",
     "author": "Vilem Novak & Contributors",
-    "version":(1,0,9),
+    "version":(1,0,10),
     "blender": (3, 6, 0),
     "location": "Properties > render",
     "description": "Generate machining paths for CNC",
