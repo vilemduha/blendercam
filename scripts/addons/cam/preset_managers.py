94d717136608a27555fa30133f05966b7de6d4bf
SpectralVectors <robmuir1@gmail.com>
2024-04-06 12:38:11 -0400

    Operation Preset Update
    Operation Presets have been updated to create a new operation by default, they will also now follow the naming convention - OP_ObjectName_OperationNumber_PresetName, to keep in line with the naming of default operations.

--- scripts/addons/cam/preset_managers.py
+++ scripts/addons/cam/preset_managers.py
@@ -67,8 +67,11 @@ class AddPresetCamOperation(AddPresetBase, Operator):
     preset_menu = "CAM_OPERATION_MT_presets"
 
     preset_defines = [
-        'import cam',
-        'o = cam.utils.setup_operation_preset()',
+        'from pathlib import Path',
+        'bpy.ops.scene.cam_operation_add()',
+        'scene = bpy.context.scene',
+        'o = scene.cam_operations[scene.cam_active_operation]',
+        "o.name = f'OP_{o.object_name}_{scene.cam_active_operation + 1}_{Path(__file__).stem}'",
     ]
 
     preset_values = [
