64b13b1b838105108b26746476e2052663dedf4a
SpectralVectors <robmuir1@gmail.com>
2024-09-05 10:01:49 -0400

    Update install_addon.py

--- scripts/addons/cam/tests/install_addon.py
+++ scripts/addons/cam/tests/install_addon.py
@@ -7,8 +7,9 @@ import tempfile
 
 INSTALL_CODE = f"""
 import bpy
-bpy.ops.extensions.package_install_files(filepath='{sys.argv[1]}')
+bpy.ops.extensions.package_install_files(filepath='{sys.argv[1]}', repo='user_default')
 bpy.ops.wm.save_userpref()
+bpy.context.scene.render.engine = 'CNCCAM_RENDER'
 """
 
 NUM_RETRIES = 10
