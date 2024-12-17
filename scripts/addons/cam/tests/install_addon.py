fc91a43fa25c387fe0fa31c8af40d683fc9fc3dd
Rob <robmuir1@gmail.com>
2024-12-17 17:29:25 -0500

    Remove debug lines

--- scripts/addons/cam/tests/install_addon.py
+++ scripts/addons/cam/tests/install_addon.py
@@ -34,7 +34,6 @@ with tempfile.TemporaryDirectory() as td:
                 text=True,
             )
             print("installed addon okay")
-            print(str(file))
             sys.exit(0)
         except subprocess.CalledProcessError as e:
             print("Install addon failed, retrying:", e)
