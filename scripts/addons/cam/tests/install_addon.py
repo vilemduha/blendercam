445a21fe98e494578db35789605793a11adda046
Rob <robmuir1@gmail.com>
2024-12-18 08:48:05 -0500

    Workflows be testy

--- scripts/addons/cam/tests/install_addon.py
+++ scripts/addons/cam/tests/install_addon.py
@@ -26,7 +26,7 @@ with tempfile.TemporaryDirectory() as td:
     for x in range(NUM_RETRIES):
         try:
             subprocess.run(
-                [shutil.which("blender"), "-b", "-P", str(file)],
+                [shutil.which("blender"), "-b", "-y", "-P", str(file)],
                 shell=False,
                 check=True,
                 stdout=subprocess.PIPE,
