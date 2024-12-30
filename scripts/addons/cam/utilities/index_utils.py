300e7e819523c9bef6981aae734866ef3e09586c
Stationär\Henrik <henrik.munktell@gmail.com>
2024-12-30 14:28:59 +0100

    Fix missing import in index_utils.py

--- scripts/addons/cam/utilities/index_utils.py
+++ scripts/addons/cam/utilities/index_utils.py
@@ -8,7 +8,7 @@ import shapely
 import bpy
 
 from .simple_utils import activate
-
+from .orient_utils import rotation_to_2_axes
 
 def prepare_indexed(o):
     """Prepare and index objects in the given collection.
